aedad54d9637ffc7af4309eafcf9d950b2f59255
Anshul-Padiyar <91671001+Anshul-Padiyar@users.noreply.github.com>
2022-11-08 16:39:11 +0530

    Update disk scheduling.cpp

--- disk scheduling.cpp
+++ disk scheduling.cpp
@@ -2,23 +2,30 @@
             -- AnshulPadiyar -- 08 Nov.2022 -- disk scheduling --
 *******************************************************************************/
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
     int RQ[100], i, n, TotalHeadMoment = 0, initial;
-    printf("Enter the number of Requests\n");
+    
+    printf("Enter the number of Requests : ");
     scanf("%d", &n);
-    printf("Enter the Requests sequence\n");
+    
+    printf("Enter the Requests sequence : \n");
     for (i = 0; i < n; i++)
+    {
         scanf("%d", &RQ[i]);
-    printf("Enter initial head position\n");
+    }
+    
+    printf("Enter initial head position : ");
     scanf("%d", &initial);
-    for (i = 0; i < n; i++){
+    
+    for (i = 0; i < n; i++)
+    {
         TotalHeadMoment = TotalHeadMoment + abs(RQ[i] - initial);
         initial = RQ[i];
     }
-    printf("Total head moment is %d \n", TotalHeadMoment);
+    printf("Total head moment is %d.\n", TotalHeadMoment);
     return 0;
 }
